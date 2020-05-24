0eeb8a7ffb0d1b42e4b810e8ed366072721d7138
Richard Diamond <richardlewisdiamond@gmail.com>
2020-05-23 23:08:39 -0600

    SplashScreen shell

---
+++ src/main/java/common/application/ApplicationSettings.java
@@ -0,0 +1,11 @@
+package common.application;
+
+import javafx.scene.image.Image;
+
+public class ApplicationSettings {
+
+    public static String VERSION = "0.1.0";
+    public static String LOGO_URL = "/theme/logo.png";
+    public static Image LOGO = new Image(LOGO_URL);
+
+}
